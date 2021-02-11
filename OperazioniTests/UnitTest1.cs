0d71f36296785b7ab2dcb57793a7a0f5230ce377
giuliapupo0@gmail.com <giuliapupo0@gmail.com>
2021-02-11 12:42:06 +0100

    Caccia all'errore

--- OperazioniTests/UnitTest1.cs
+++ OperazioniTests/UnitTest1.cs
@@ -15,6 +15,24 @@ namespace OperazioniTests
             Assert.AreEqual(valore_aspettato, resp);
         }
         [TestMethod]
+        public void TestCalcoloMisterioso1_0()
+        {
+            int n = 1;
+            long valore_aspettato = 2;
+            long resp = Gestione.CalcoloMisterioso(n);
+            Assert.AreEqual(valore_aspettato, resp);
+        }
+        [TestMethod]
+        public void TestCalcoloMisterioso1_1()
+        {
+            int n = 0;
+            long valore_aspettato = 1;
+            long resp = Gestione.CalcoloMisterioso(n);
+            Assert.AreEqual(valore_aspettato, resp);
+        }
+
+
+        [TestMethod]
         public void TestCalcoloMisterioso2()
         {
             int n = 3;
@@ -22,12 +40,43 @@ namespace OperazioniTests
             long resp = Gestione.CalcoloMisterioso2(n);
             Assert.AreEqual(valore_aspettato, resp);
         }
+        [TestMethod]
+        public void TestCalcoloMisterioso2_0()
+        {
+            int n = 5;
+            long valore_aspettato = -360;
+            long resp = Gestione.CalcoloMisterioso2(n);
+            Assert.AreEqual(valore_aspettato, resp);
+        }
+        [TestMethod]
+        public void TestCalcoloMisterioso2_1()
+        {
+            int n = 0;
+            long valore_aspettato = 0;
+            long resp = Gestione.CalcoloMisterioso2(n);
+            Assert.AreEqual(valore_aspettato, resp);
+        }
+
 
         [TestMethod]
         public void TestMisurazioneMisteriosa()
         {
             double[] valori = new double[4] { 4, 6, 7, -9.1 };
             double valore_aspettato = 3;
+            double resp = Gestione.MisurazioneMisteriosa(valori);
+            Assert.AreEqual(valore_aspettato, resp);
+        }
+        public void TestMisurazioneMisteriosa1()
+        {
+            double[] valori = new double[5] { 5, 10, 21, 12, 25 };
+            double valore_aspettato = 0;
+            double resp = Gestione.MisurazioneMisteriosa(valori);
+            Assert.AreEqual(valore_aspettato, resp);
+        }
+        public void TestMisurazioneMisteriosa2()
+        {
+            double[] valori = new double[6] { -4, 11, -9, 8, 16, -2 };
+            double valore_aspettato = 5;
             double resp = Gestione.MisurazioneMisteriosa(valori);
             Assert.AreEqual(valore_aspettato, resp);
         }
